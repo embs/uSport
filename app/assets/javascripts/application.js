dc0e00c71f5a9ae5c5309e2a49a3e6496cf1f45b
Eduardo Macedo <edumcd@gmail.com>
2013-03-08 07:32:24 -0300

    corrigido todos os problemas de resolucoes de telas

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -60,23 +60,23 @@ function tbAdaptor(){
  
 	if (docWidth>=1600){
 		$("div[tb-g-1600]").each(function(index){
-			$(this).removeClass("span1 span2 span3 span4 span5 span6 span7 span8 span9 span10 span11 span12 hide").addClass($(this).attr("tb-g-1600"));
+			$(this).removeClass("row-fluid span1 span2 span3 span4 span5 span6 span7 span8 span9 span10 span11 span12 hide").addClass($(this).attr("tb-g-1600"));
 		});
 	}else if (docWidth>=1400){
 		$("div[tb-g-1400]").each(function(index){
-			$(this).removeClass("span1 span2 span3 span4 span5 span6 span7 span8 span9 span10 span11 span12 hide").addClass($(this).attr("tb-g-1400"));
+			$(this).removeClass("row-fluid span1 span2 span3 span4 span5 span6 span7 span8 span9 span10 span11 span12 hide").addClass($(this).attr("tb-g-1400"));
 		});
 	}else if (docWidth>=1200){
 		$("div[tb-g-1200]").each(function(index){
-			$(this).removeClass("span1 span2 span3 span4 span5 span6 span7 span8 span9 span10 span11 span12 hide").addClass($(this).attr("tb-g-1200"));
+			$(this).removeClass("row-fluid span1 span2 span3 span4 span5 span6 span7 span8 span9 span10 span11 span12 hide").addClass($(this).attr("tb-g-1200"));
 		});
 	}else if(docWidth>=980){
 		$("div[tb-d]").each(function(index){
-			$(this).removeClass("span1 span2 span3 span4 span5 span6 span7 span8 span9 span10 span11 span12 hide").addClass($(this).attr("tb-d"));
+			$(this).removeClass("row-fluid span1 span2 span3 span4 span5 span6 span7 span8 span9 span10 span11 span12 hide").addClass($(this).attr("tb-d"));
 		});
 	}else if(docWidth>=768){
 		$("div[tb-768-980]").each(function(index){
-			$(this).removeClass("span1 span2 span3 span4 span5 span6 span7 span8 span9 span10 span11 span12 hide").addClass($(this).attr("tb-768-980"));
+			$(this).removeClass("row-fluid span1 span2 span3 span4 span5 span6 span7 span8 span9 span10 span11 span12 hide").addClass($(this).attr("tb-768-980"));
 		});
 	}
  
