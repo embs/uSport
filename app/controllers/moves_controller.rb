97a32d8666d5a4a7a86e77bb2ea93d5f3a4108fd
embs <embs@cin.ufpe.br>
2013-03-20 16:41:54 -0300

    Dá feedback ao usuário após criação de jogada através da página de criação de jogada. Closes #100

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -53,6 +53,7 @@ class MovesController < ApplicationController
       @match.value2 = @match.value2 + points
     end
     @match.save
+    flash[:notice] = "Jogada criada!" if @move.valid?
 
     respond_to do |format|
       format.html { render 'create.js.erb' }
