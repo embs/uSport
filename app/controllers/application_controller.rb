29a635ecbb7eca697eefe7fcb2ab2fee306f0e7e
Marcosbarros <marcosadvt@gmail.com>
2013-03-21 08:46:56 -0300

    Altera redirecionamento do devise após logar e deslogar para mesma página

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
+  # \/ deixa o cara na mesma página após logar ou deslogar
+  def after_sign_out_path_for(resource_or_scope)
+  request.referrer
+  end
+  def after_sign_in_path_for(resource_or_scope)
+  request.referrer
+  end
 end
