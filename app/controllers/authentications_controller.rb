9e4d5ad7be87420f48d4b5158959fe7a702a878b
embs <embs@cin.ufpe.br>
2013-03-21 11:44:43 -0300

    Associa conta do Facebook quando já existe uma conta criada com o mesmo e-mail. Closes #70

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -13,12 +13,12 @@ class AuthenticationsController < ApplicationController
       flash[:notice] = "Logado com sucesso."
       sign_in_and_redirect(:user, authentication.user)
     else
-      # Autenticação inexistente -> cria usuário e loga
-      user = User.create_with_omniauth(auth)
+      # Autenticação inexistente -> cria usuário ou associa conta e loga
+      user = User.find_by_email(auth['info']['email']) || User.create_with_omniauth(auth)
       if user
-        Authentication.create(:provider => auth['provider'], :user_id => user.id,
-                              :uid => auth[:uid])
-        flash[:notice] = "Conta criada a partir da conta do Facebook."
+        Authentication.create(provider: auth['provider'], user_id: user.id,
+                              uid: auth['uid'])
+        flash[:notice] = "Você entrou com a sua conta do Facebook."
         sign_in_and_redirect(:user, user)
       else
         flash[:error] = "Não foi possível logar com o Facebook."
