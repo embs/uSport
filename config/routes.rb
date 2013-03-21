77d6b1637c48bd115e159d752741d738439b2a1c
embs <embs@cin.ufpe.br>
2013-03-21 10:25:08 -0300

    Remove a rota para players#index. #35

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ USport::Application.routes.draw do
 
   resources :teams
 
-  resources :players
+  resources :players, except: :index
 
   resources :users, :only => [:index, :edit, :update] do
     resources :favorite_channels, only: [:index, :create, :destroy]
