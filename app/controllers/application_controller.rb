2751d3d339ab508af2b1f94bdd8daf514e6990f7
embs <embs@cin.ufpe.br>
2013-03-24 22:53:50 -0300

    Faz voltar para página onde estava antes de logar com o Devise (logar com o FB já estava funcionando). #88

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     request.env['omniauth.origin'] || request.referer || root_path
   end
-  
+
   def after_sign_in_path_for(resource_or_scope)
-    request.env['omniauth.origin'] || session.delete(:return_to) || root_path
+    request.env['omniauth.origin'] || request.referer || session.delete(:return_to) || root_path
   end
 end
