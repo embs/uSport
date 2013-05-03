b4cb0729da425031a02508cf4e38fa6b3ac4f795
embs <embs@cin.ufpe.br>
2013-05-03 01:50:39 -0300

    Adiciona estrutura do índice da Central de Ajuda (incluindo Javascript para o dinamismo do conteúdo)

--- app/assets/javascripts/help/index.js
+++ app/assets/javascripts/help/index.js
@@ -1,15 +1,16 @@
 $(function() {
   $('.expander').click(function() {
     var $this = $(this);
+    var $id = $this.id
     var $container = $this.parents('.page-header');
-    var $listItem = $container.find('#sign-up');
+    var $listItem = $container.find('.expand-help#' + $this[0].id);
 
     if($listItem.hasClass('open')) {
       $listItem.slideUp(150, 'swing');
+      $this.find('i').removeClass('icon-chevron-down').addClass('icon-chevron-right');
     } else {
       $listItem.slideDown(150, 'swing');
-      var $icon = $listItem.find('i');
-      $icon.html('');
+      $this.find('i').removeClass('icon-chevron-right').addClass('icon-chevron-down');
     }
 
     $listItem.toggleClass('open');
